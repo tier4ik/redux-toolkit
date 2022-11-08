0b66bc59cd9a018db0933c7e56fc9bc5fd6af767
Max Shevchencko <tier4ik@gmail.com>
2022-11-08 09:11:52 +0300

    Added new component "PostsExcerpt"

---
+++ src/components/Posts/PostsExcerpt.tsx
@@ -0,0 +1,24 @@
+import type { PostStateType } from "./postsSlice";
+import PostAuthor from "./PostAuthor";
+
+import style from "./Posts.module.css";
+
+type Props = {
+  post: PostStateType;
+};
+
+const PostsExcerpt: React.FC<Props> = ({ post }) => {
+  return (
+    <article className={style.post}>
+      <h2 className={style.post__header}>{post.title}</h2>
+      <p className={style.post__content}>
+        {post.content.length > 100
+          ? `${post.content.substring(0, 100)}...`
+          : post.content}
+      </p>
+      <PostAuthor userId={post.authorId} />
+    </article>
+  );
+};
+
+export default PostsExcerpt;
